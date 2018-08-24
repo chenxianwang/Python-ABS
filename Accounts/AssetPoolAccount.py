4165db80d4faabaff780b00f1b1fc03ab365dd3f
chenxianwang <yxchenw@163.com>
2018-08-24 23:46:19 +0800

    Add files via upload

--- Accounts/AssetPoolAccount.py
+++ Accounts/AssetPoolAccount.py
@@ -22,42 +22,48 @@ logger = get_logger(__name__)
 
 class AssetPoolAccount():
     
-    def __init__(self,ACFa):
+    def __init__(self,ACF,ACFa):
+        
+        self.original_principal = {k:ACF[pd.to_datetime(ACF['date_recycle']) == k]['amount_principal'].sum() for k in dates_recycle}
+        self.original_principal[dates_recycle[0]] = ACF['amount_principal'][pd.to_datetime(ACF['date_recycle']) <= dates_recycle[0]].sum()
+        self.original_interest = {k:ACF[pd.to_datetime(ACF['date_recycle']) == k]['amount_interest'].sum() for k in dates_recycle}
+        self.original_interest[dates_recycle[0]] = ACF['amount_interest'][pd.to_datetime(ACF['date_recycle']) <= dates_recycle[0]].sum()
         
         self.recylce_principal = {k:ACFa[pd.to_datetime(ACFa['date_recycle']) == k]['amount_recycle_principal'].sum() for k in dates_recycle}
         self.recylce_principal[dates_recycle[0]] = ACFa['amount_recycle_principal'][pd.to_datetime(ACFa['date_recycle']) <= dates_recycle[0]].sum()
-        
         self.recylce_interest = {k:ACFa[pd.to_datetime(ACFa['date_recycle']) == k]['amount_recycle_interest'].sum() for k in dates_recycle}
         self.recylce_interest[dates_recycle[0]] = ACFa['amount_recycle_interest'][pd.to_datetime(ACFa['date_recycle']) <= dates_recycle[0]].sum()
 
-        self.principal_total = {}
+        self.principal_original = {}
+        self.principal_actual = {}
         self.principal_to_pay = {}
         self.principal_to_buy = {}
+        self.principal_to_loss = {}
         
-        self.interest_total = {}
+        self.interest_original = {}
+        self.interest_actual = {}
         self.interest_to_pay = {}
         self.interest_to_buy = {}
+        self.interest_loss = {}
     
     def available_principal(self):
         
-        self.principal_total = self.recylce_principal
+        self.principal_original = self.original_principal
+        self.principal_actual = self.recylce_principal
         self.principal_to_pay = deepcopy(self.recylce_principal)
         self.principal_to_buy = {}
         for k in dates_recycle:
             self.principal_to_buy[k] = 0
-        return self.principal_total,self.principal_to_pay,self.principal_to_buy
+            self.principal_to_loss[k] = 0
+        return self.principal_actual,self.principal_to_pay,self.principal_to_buy,self.principal_to_loss,self.principal_original
 
     def available_interest(self):
         
-        self.interest_total = self.recylce_interest
+        self.interest_original = self.original_interest
+        self.interest_actual = self.recylce_interest
         self.interest_to_pay = deepcopy(self.recylce_interest)
         self.interest_to_buy = {}
         for k in dates_recycle:
             self.interest_to_buy[k] = 0
-        return self.interest_total,self.interest_to_pay,self.interest_to_buy
-    
-    def get_PrincipalBalance(self,date_check):
-        return self.recylce_principal[date_check]
-    
-    def get_InterestBalance(self,date_check):
-        return self.recylce_interest[date_check]
+            self.interest_loss[k] = 0
+        return self.interest_actual,self.interest_to_pay,self.interest_to_buy,self.interest_loss,self.interest_original
