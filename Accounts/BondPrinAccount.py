bd8fdb3ca806409e0085eed35b3d3beaa567cfb8
chenxianwang <yxchenw@163.com>
2018-10-23 17:47:32 +0800

    Add files via upload

--- Accounts/BondPrinAccount.py
+++ Accounts/BondPrinAccount.py
@@ -27,24 +27,31 @@ class BondPrinAccount():
         self.balance = {}
         self.receive = {}
     
-    def pay_then_ToNext(self,date_pay,amount_available):
+    def pay_then_ToNext(self,date_pay,amount_available,amount_reserve):
                     
         if self.bondinfo['amount'] == 0:
             self.balance[date_pay] = 0
             self.receive[date_pay] = 0
-            return amount_available
+            return amount_available,amount_reserve
         
         elif amount_available < -0.0001:
             logger.info('self.bondinfo[amount] of {0} is {1}'.format(self.name_bond,self.bondinfo['amount']))
             logger.info('amount_available for {0} on {1} is {2}: '.format(self.name_bond,date_pay,amount_available))
             #sys.exit("!!!!!!!!!    Can not cover payment      !!!!!!!")
-            return amount_available
+            #self.bondinfo['amount'] -= amount_available
+            self.balance[date_pay] = self.bondinfo['amount']
+            self.receive[date_pay] = 0 #-amount_available
+            
+            amount_reserve += amount_available
+            amount_available = 0
+            
+            return amount_available,amount_reserve
         
         elif self.bondinfo['amount'] >= amount_available:
             self.bondinfo['amount'] -= amount_available
             self.balance[date_pay] = self.bondinfo['amount']
             self.receive[date_pay] = amount_available
-            return 0
+            return 0,amount_reserve
         else: 
             #logger.info('date_pay for {0} is {1}'.format(self.name_bond,date_pay))
             self.receive[date_pay] = self.bondinfo['amount']
@@ -54,7 +61,7 @@ class BondPrinAccount():
             if amount_available < self.receive[date_pay] :
                 logger.info('amount_available is {0}, receive[date_pay] is {1}'.format(amount_available,receive[date_pay]))
             
-            return amount_available - self.receive[date_pay]
+            return amount_available - self.receive[date_pay],amount_reserve
            
     def iBalance(self,date_pay):
         #logger.info('date_pay for {0} is {1}'.format(self.name_bond,date_pay))
